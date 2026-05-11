a803716cdbdbd6c91879f6b74711084fd63de391
Pardeep Singh <pard6508@gmail.com>
2026-05-11 09:48:24 +0530

    Fixed vercel routing

--- src/Pages/AllJobs.jsx
+++ src/Pages/AllJobs.jsx
@@ -32,7 +32,7 @@ export default function AllJobs() {
                 setEntries(data.allJobs)
                 
             } catch (err) {
-                console.log("err")
+                console.log(err)
             }
             
         }
